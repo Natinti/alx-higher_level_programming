79f704dc0f49338928e764c6a8065f16e74fa08f
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-03-14 21:26:46 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum of a and b, """
+    from add_0 import add
 
-from add_0 import add
-
-result = add(a, b)
-
-print(f"{a} + {b} = {result}")
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
