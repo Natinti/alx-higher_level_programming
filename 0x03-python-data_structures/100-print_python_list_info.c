ca680ac761aa2655fd357b66059d44e35977017c
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-03-18 22:47:18 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,12 +11,11 @@
 
 void print_python_list_info(PyObject *p)
 {
-	long int size = PyList_Size(p);
 	int i;
 	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*} Size of the Python List = %li\n", Py_SIZE(p));
-	printf("[*] Allocated = %li\n", obj->allocated);
+	printf("[*} Size of the Python List = %lu\n",Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", obj->allocated);
 	for (i = 0; i < PY_SIZE(p); i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+		printf("Element %d: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
