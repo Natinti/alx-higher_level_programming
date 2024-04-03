5662d957abc0e4b83fae3b55cb035650d4e73eae
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-04-03 02:52:13 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """ Print the x elements of a list. 
+    """Print the x elements of a list.
 
     Args: my_list(list): This is the list to print
     elements from x (int): The number of elements of my_list to print.
