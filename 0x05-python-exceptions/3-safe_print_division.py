42bcec1c91180da64b0f36f0d1c3c708059904eb
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-04-03 21:06:23 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     """Returns the division of a and b."""
     try:
-        div = a / b
+        div = a/b
     except(ZeroDivisionError, FloatingPointError):
         div = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(div))
     return div
