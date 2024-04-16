0941a6b4dc96b2bff5bf9f3c11d2ee963dd49643
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-04-15 21:28:05 -0700

    1-list_files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list
+"""
+
+
+
+class MyList(list):
+    """Inherits from list"""
+    def print_sorted(self):
+        """prints the list, but sorted
+        (ascending sort)
+        """
+        print(sorted(self))
+
