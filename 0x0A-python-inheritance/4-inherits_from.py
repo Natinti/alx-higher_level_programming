adbc316df70a01d3e9acbebad101abdedf0fc50a
Nana Twumasi-Ntiamoah <nanatwumasintiamoah@gmail.com>
2024-04-16 23:22:50 -0700

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,6 @@ def inherits_from(obj, a_class):
     that onherited (directly or indirectly) from the specified class;
     otherwise False.
     """
-    if type(obj) != a_class:
-        return issubclass(type(obj), a_class)
-    else:
+    if type(obj) is a_class:
         return False
+    return issubclass(type(obj), a_class)
